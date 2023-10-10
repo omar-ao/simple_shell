22cb68249aed425ca7af1cc70868b5d2577bd281
Mai2aa <mysaadil0@gmail.com>
2023-10-09 18:28:18 -0700

    add prompt

---
+++ prompt.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * prompt - prints the prompt
+ *
+ */
+void prompt(void)
+{
+	char *line = NULL;
+	size_t n = 0;
+	ssize_t nread;
+
+	while (1)
+	{
+		nread = getline(&line, &n, stdin);
+		if (nread == -1)
+			break;
+		write(STDOUT_FILENO, line, nread);
+	}
+	free(line);
+}
