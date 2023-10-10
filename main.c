d8c0b85309ca52e2b1bf209211c29c48742e0494
Mai2aa <mysaadil0@gmail.com>
2023-10-09 18:48:00 -0700

    main function

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	if (!isatty(STDIN_FILENO))
+	{
+		readline();
+	}
+	else
+	{
+		prompt();
+	}
+	return (0);
+}
