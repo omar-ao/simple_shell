03abef77823b5bfafe935f77f21e15ce1d0c8315
Mai2aa <mysaadil0@gmail.com>
2023-10-16 19:50:55 -0700

    Handle errors

--- print_err.c
+++ print_err.c
@@ -2,11 +2,14 @@
 
 /**
  * print_err - Prints error message to the standard error
- *
+ * @cmd_name: command
+ * @err_msg: error message
+ * @err_count: error count
  */
 void print_err(char *cmd_name, char *err_msg, int err_count)
 {
 	char *name = _getenv("_");
 
-	dprintf(STDERR_FILENO, "%s: %d: %s: %s\n", name, err_count, cmd_name, err_msg);
+	dprintf(STDERR_FILENO, "%s: %d: %s: %s\n",
+			name, err_count, cmd_name, err_msg);
 }
